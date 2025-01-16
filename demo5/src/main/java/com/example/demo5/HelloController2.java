8a57d1bc1c03e33bffdfa601a70db4a370dc5f90
DONJUNIOR916 <junior.donfack@institutsaintjean.org>
2025-01-16 11:32:29 +0100

    Add files via upload
    page 8

---
+++ demo5/src/main/java/com/example/demo5/HelloController2.java
@@ -0,0 +1,18 @@
+package com.example.demo5;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class HelloController2 {
+
+    @FXML
+    private Button cancelBtn;
+
+    @FXML
+    private Button confirmBtn;
+
+    @FXML
+    private VBox loanBoxFill;
+
+}
