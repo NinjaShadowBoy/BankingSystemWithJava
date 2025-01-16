5cb9cfcbe6fc91a231e3f1e12c2a532f62b9e51c
Alex Nelson Ryan <142633593+NinjaShadowBoy@users.noreply.github.com>
2025-01-16 22:21:48 +0100

    Created a loadDB function to load info from the Database

--- oopPage10/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.ooppage10 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.ooppage10 to javafx.fxml;
-    exports com.example.ooppage10;
-}
--- oopPage9/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.ooppage9 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.ooppage9 to javafx.fxml;
-    exports com.example.ooppage9;
-}
